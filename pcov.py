ef79aaecb11a2c53d9ba435798bd5dfcb1cd58dd
potatmen <ashatokhi2-c@my.cityu.edu.hk>
2023-04-11 17:52:49 +0900

    Taken branch counting added

--- pcov.py
+++ pcov.py
@@ -1,22 +1,21 @@
 import ast
 import argparse
-import sys, subprocess
+import sys
 import traceback
 from copy import deepcopy
 
-exec_lines = set()
+exec_lines = list()
 
 
 def trace_file_execution(file_name, args):
     with open(file_name) as f:
         code = compile(f.read(), file_name, 'exec')
-    lines_executed = set()
 
     def trace_lines(frame, event, arg):
         if event == 'line':
             line_no = frame.f_lineno
-            if line_no not in exec_lines:
-                exec_lines.add(line_no)
+            if line_no:
+                exec_lines.append(line_no)
         return trace_lines
 
     try:
@@ -33,17 +32,28 @@ def trace_file_execution(file_name, args):
 BRANCHES_COUNT = 0
 CONDITIONS_COUNT = 0
 lines_with_stm = set()
+lines_with_branch = set()
+exceptions = set()
 br_nodes = [ast.If, ast.For, ast.While, ast.IfExp, ast.ListComp]
 
 class MyVisitor(ast.NodeVisitor):
+	
 	def generic_visit(self, node):
 		global BRANCHES_COUNT
+        
 		if isinstance(node, ast.stmt):
 			lines_with_stm.add(node.lineno)
 		if type(node) in br_nodes:
+            
 			BRANCHES_COUNT += 2
+        	
+			lines_with_branch.add(node.lineno)
+        
 		if type(node) == ast.Try:
 			BRANCHES_COUNT += len(node.handlers)
+		
+		if type(node) == ast.ExceptHandler:
+			exceptions.add(node.lineno)
 		super().generic_visit(node)
 
 if __name__ == '__main__':
@@ -57,6 +67,7 @@ if __name__ == '__main__':
 	lines = open(target, "r").readlines()
 	root = ast.parse("".join(lines), target)
 	visitor = MyVisitor()
+    
 	visitor.visit(root)
 	# instrument the target script
 
@@ -67,18 +78,43 @@ if __name__ == '__main__':
 	
 	# execute the instrumented target script
 	trace_file_execution(target, args.remaining)
+                  
 
-	executed_statements = len(lines_with_stm.intersection(exec_lines))
+	#print("Exec lines: " , exec_lines)
+    
+	#print("Branch lines: " , lines_with_branch)
+
+	executed_statements = len(lines_with_stm.intersection(set(exec_lines)))
 
 	all_statements = len(lines_with_stm)
     
-	val = 0
+	stm_prc = 0
 	if all_statements != 0:
-		val = (executed_statements/all_statements) * 100
+		stm_prc = (executed_statements/all_statements) * 100
+                
+	pairs = set()
+	
+	exec_branches = 0
 
+	for i in range(len(exec_lines)):
+    	
+		if exec_lines[i] in lines_with_branch:
+			if i + 1 < len(exec_lines):
+				pairs.add((exec_lines[i], exec_lines[i + 1]))
+			else:
+				pairs.add((exec_lines[i], None))
+		elif exec_lines[i] in exceptions and i + 1 < len(exec_lines) and exec_lines[i + 1] == exec_lines[i] + 1:
+			pairs.add((exec_lines[i], exec_lines[i + 1]))
+
+	
+	exec_branches = len(pairs)
+
+	bra_prc = 0
+	if BRANCHES_COUNT != 0:
+		bra_prc = (exec_branches/BRANCHES_COUNT) * 100
 	print("=====================================")
-	print(f"Statement Coverage: {val:.2f}% ({executed_statements} / {all_statements})")
-	print(f"Branch Coverage: {0.00}% (0 / {BRANCHES_COUNT})")
+	print(f"Statement Coverage: {stm_prc:.2f}% ({executed_statements} / {all_statements})")
+	print(f"Branch Coverage: {bra_prc:.2f}% ({exec_branches} / {BRANCHES_COUNT})")
 	print(f"Condition Coverage: {0.00}% (0 / {CONDITIONS_COUNT})")
 
 	if args.verbose:
